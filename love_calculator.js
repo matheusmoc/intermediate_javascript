bc32e92d93651f462c9c39eb038f66ea95b5d433
matheusmoc <matheusandrade009@gmail.com>
2022-02-07 12:33:44 -0300

    IBM CALCULATO/LOVE CALCULATOR

---
+++ love_calculator.js
@@ -0,0 +1,18 @@
+prompt("what is your name?")
+prompt("what is their name?")
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+console.log(loveScore); 
+
+
+if(loveScore >= 80 ){
+    console.log("Your love score is "+loveScore+"%" + " Wow!!! You're a lucky person")
+} else{
+    alert("Your love score is "+loveScore+"%");
+}
+
+ 
+
+
+

5c1e811d6f3fe9617349d5cb35241103a35fd224
matheusmoc <matheusandrade009@gmail.com>
2022-02-08 13:03:49 -0300

    FIZZBUZZ

---
+++ collections_verification.js
@@ -0,0 +1,6 @@
+var guestList = ["Singed", "Yorick", "Xayah", "Vi"];
+console.log(guestList[0]);
+
+var champVerification = prompt('what is your best champion?');
+guestList.includes(champVerification);
+
